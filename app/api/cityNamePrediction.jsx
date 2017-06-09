b7a385c17811584f32fb1b828430268212cfbfe2
Atul Kotwale <atulkotwale@Atuls-MacBook-Pro.local>
2017-06-09 16:59:56 -0700

    updated bundle js

--- app/api/cityNamePrediction.jsx
+++ app/api/cityNamePrediction.jsx
@@ -3,11 +3,14 @@ var axios = require('axios');
 const PORT = process.env.PORT || 3001;
 const CITY_NAMES_PREDICTION_URL ='https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&language=pt_BR&key=AIzaSyD64nVQSEfh3gCP0GwKsgeLReHuXWK2iZ8';
 
+const HOST = process.env.HOST || localhost;
+
 module.exports = {
     getCityNames : function(location) {
       console.log("location needs to search -" + location);
       var encodedLocation = encodeURIComponent(location);
-      var requestUrl = `${CITY_NAMES_PREDICTION_URL}&input=${encodedLocation}`;
+      //var requestUrl = `${CITY_NAMES_PREDICTION_URL}&input=${encodedLocation}`;
+      var requestUrl = `https://${HOST}:${PORT}/getPrediction?location${encodedLocation}`
       console.log("Url for calling the predictions-" + requestUrl);
       return axios.get(requestUrl).then(function(res){
         console.log("got predictions " + res.data.predictions);
--- public/bundle.js
+++ public/bundle.js
@@ -28548,11 +28548,14 @@
 	var PORT = process.env.PORT || 3001;
 	var CITY_NAMES_PREDICTION_URL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&language=pt_BR&key=AIzaSyD64nVQSEfh3gCP0GwKsgeLReHuXWK2iZ8';
 
+	var HOST = process.env.HOST || localhost;
+
 	module.exports = {
 	  getCityNames: function getCityNames(location) {
 	    console.log("location needs to search -" + location);
 	    var encodedLocation = encodeURIComponent(location);
-	    var requestUrl = CITY_NAMES_PREDICTION_URL + '&input=' + encodedLocation;
+	    //var requestUrl = `${CITY_NAMES_PREDICTION_URL}&input=${encodedLocation}`;
+	    var requestUrl = 'https://' + HOST + ':' + PORT + '/getPrediction?location' + encodedLocation;
 	    console.log("Url for calling the predictions-" + requestUrl);
 	    return axios.get(requestUrl).then(function (res) {
 	      console.log("got predictions " + res.data.predictions);
